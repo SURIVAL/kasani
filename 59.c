952e764a588480bd74b4ec4291b5fbf316a63d64
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-01-24 16:21:20 +0530

    59.c

---
+++ 59.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a[10],n, i;
+    printf("Enter ten values:");
+    for (i = 0; i < 10; i++)
+    {
+    scanf("%d", &a[n]);
+    }
+    i = a[0];
+    for (i = 0; i < 10; i++)
+    {
+    if (a[n] > i) 
+    {
+    i= a[n];
+    }
+    }
+    printf(" Greatest of ten numbers is %d", i);
+    return 0;
+}
