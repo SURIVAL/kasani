ab672d0d876a749c46cc1e22a1d341213062b636
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-01-24 16:08:06 +0530

    57.c

---
+++ 57.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+int x1,x2,temp;
+printf("enter the before swap &x1,&x2");
+temp=x1;
+x1=x2;
+x2=x1;
+printf("enter the after swap &x1,&x2");
+}
