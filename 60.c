2a3e62d33891ef36ad88958f1f423f06356677d8
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-01-24 16:39:28 +0530

    60.c

---
+++ 60.c
@@ -0,0 +1,15 @@
+#includ<stdio.h>
+#include<conio.h>
+void main()
+{
+int x=0,n=1,i;
+    for(i=0;i<=5;++i)
+    {
+        printf("%d",x);
+        s=x+n;
+        x=n;
+        x=s;
+        
+        
+    }
+}
