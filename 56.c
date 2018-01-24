cb6452cd17e499eb2dfe61232cebc33ba3886a81
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-01-24 16:46:51 +0530

    56.c

---
+++ 56.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int d;
+char s[10];
+printf("enter the number");
+scanf("%d",&d);
+printf("enter the string ");
+scanf("%s",&s[10]);
+g=s[10].d;
+if(((g>='a')&&(g<='z'))&&((g>='A')&&(g<='Z'))&&((g>='0'))&&((g<='9')))
+{
+printf("yes");
+}
